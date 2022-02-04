e483f800f4d25c2658b764642da74e60a075244a
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2022-02-03 20:50:34 -0600

    Create Города_и_дороги.py

---
+++ Уровень3/Уровень3А/Задачи_на_простые_графы/Города_и_дороги.py
@@ -0,0 +1,12 @@
+#https://algoprog.ru/material/p174
+
+n = int(input())
+cnt = 0
+seen = set()
+for i in range(n):
+  array = [*map(int, input().split())]
+  for j in range(n):
+    if array[j] == 1 and (i, j) not in seen and (j, i) not in seen:
+      seen.add((i, j))
+      cnt += 1
+print(cnt)
